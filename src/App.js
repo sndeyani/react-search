50f0dc80a0d2867099fd7c576883947e0582da60
anisndeyan <ani@mergedup.com>
2022-10-19 22:18:33 +0400

    simple serach

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useMemo } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [data, setData] = useState([])
+	const [query, setQuery] = useState('')
+	const ref = useRef()
+
+	const filtered = useMemo(() => {
+		return data.filter(item => item.toLowerCase().includes(query))
+	}, [query, data])
+
+	const submit = (e) => {
+		e.preventDefault()
+		const {value} = ref.current;
+		if(value === '') return;
+		setData(prevState => ([...prevState, value]))
+		ref.current.value = ''
+	}
+
+	const handleChange = (e) => {
+		let id;
+		if(id) clearTimeout(id)
+		id = setTimeout(() => setQuery(e.target.value), 100)
+	}
+
+  	return (
+		<div style={{width: 500, margin: 'auto'}} >
+			<h3>Search </h3> 
+			<input onChange={handleChange} type='text'/>	
+			<h3>Add</h3>
+			<form onSubmit={submit}>
+				<input ref={ref} type='text'/>
+				<button type='submit'>Add items</button>
+			</form>
+			<h3>Items</h3>
+			<div>
+				{filtered.map((item, i) => (
+					<p key={item + i}>{item}</p>
+				) )}
+			</div>
+		
+		</div>
+  	);
 }
 
 export default App;
